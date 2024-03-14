19cf8d95a639a396dbe3ab6acb64e9186d2049af
mitukou1109 <mitukou1109@gmail.com>
2024-03-14 14:17:12 +0900

    outer iteration

--- fdgmres.m
+++ fdgmres.m
@@ -1,4 +1,4 @@
-function W = fdgmres(DF, g, W0, w, omega, iter, tol)
+function W = fdgmres(DF, g, W0, w, omega, iter)
 
     arguments
         DF
@@ -7,41 +7,41 @@ function W = fdgmres(DF, g, W0, w, omega, iter, tol)
         w
         omega
         iter
-        tol = 1e-6
     end
 
-    r0 = g - DF(W0, w, omega);
-
-    if norm(r0) <= tol
-        W = W0;
-        return
-    end
-
-    V = zeros(numel(W0), iter + 1);
-    V(:, 1) = r0 / norm(r0);
-    H = zeros(iter + 1, iter);
+    W = W0;
     x = reshape(W0, [], 1);
+    m = numel(W0);
 
-    for j = 1:iter
-        Avj = DF(reshape(V(:, j), size(W0)), 0, 0);
+    for k = 1:iter
+        V = zeros(m, m + 1);
+        H = zeros(m + 1, m);
 
-        for i = 1:j
-            H(i, j) = Avj.' * V(:, i);
+        r0 = g - DF(W, w, omega);
+        V(:, 1) = r0 / norm(r0);
+
+        for j = 1:m
+            Avj = DF(reshape(V(:, j), size(W0)), w, omega);
+
+            for i = 1:j
+                H(i, j) = Avj.' * V(:, i);
+            end
+
+            s = 0;
+
+            for i = 1:j
+                s = s + H(i, j) * V(:, i);
+            end
+
+            vHat = Avj - s;
+
+            H(j + 1, j) = norm(vHat);
+            V(:, j + 1) = vHat / H(j + 1, j);
         end
 
-        s = 0;
-
-        for i = 1:j
-            s = s + H(i, j) * V(:, i);
-        end
-
-        vHat = Avj - s;
-
-        H(j + 1, j) = norm(vHat);
-        V(:, j + 1) = vHat / H(j + 1, j);
+        y = lsqminnorm(H, norm(r0) * eye(m + 1, 1));
+        x = x + V(:, 1:m) * y;
+        W = reshape(x, size(W0));
     end
 
-    y = lsqminnorm(H, norm(r0) * eye(iter + 1, 1));
-    x = x + V(:, 1:iter) * y;
-    W = reshape(x, size(W0));
 end
