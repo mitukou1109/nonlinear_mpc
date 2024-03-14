8d17ef801ac07b8f70074ac232dcb921c6163a28
mitukou1109 <mitukou1109@gmail.com>
2024-03-14 19:10:43 +0900

    fix Arnoldi algorithm

--- fdgmres.m
+++ fdgmres.m
@@ -21,13 +21,13 @@ function W = fdgmres(DF, g, W0, w, omega, iter)
         V(:, 1) = r0 / norm(r0);
 
         for j = 1:m
-            Avj = DF(reshape(V(:, j), size(W0)), w, omega);
+            Avj = DF(reshape(V(:, j), size(W0)), 0, 0);
 
             for i = 1:j
                 H(i, j) = Avj.' * V(:, i);
             end
 
-            s = 0;
+            s = zeros(size(Avj));
 
             for i = 1:j
                 s = s + H(i, j) * V(:, i);
